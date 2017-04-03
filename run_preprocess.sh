b680ea86997d96e410de54ffa5afef60b287b6d8
norikinishida <norikinishida@gmail.com>
2017-04-03 15:50:14 +0900

    fix

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -6,10 +6,10 @@ OUTPUT=./output
 python preprocess.py \
     --input $INPUT \
     --output $OUTPUT \
-    --lowercase True \
-    --replace_digits True \
-    --append_eos True \
-    --replace_rare True \
+    --lowercase 1 \
+    --replace_digits 1 \
+    --append_eos 1 \
+    --replace_rare 1 \
     --prune_at 1000000 \
     --min_count 0 
 
