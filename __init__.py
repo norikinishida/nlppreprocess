22f35877e675b73538c970107effc8c6c5980b80
norikinishida <norikinishida@gmail.com>
2017-04-09 19:54:40 +0900

    fix

--- __init__.py
+++ __init__.py
@@ -1,7 +1,1 @@
-from utils import utils
-from lowercase import lowercase
-from tokenizer import tokenizer_with_nltk
-from replace_digits import replace_digits
-from append_eos import append_eos
-from replace_rare_words import replace_rare_words
-from create_word_ids import create_word_ids
+import utils
