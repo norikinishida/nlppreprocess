f5f34eb3436aa21afba34c4393dd5a19b062720b
norikinishida <norikinishida@gmail.com>
2017-09-23 12:47:34 +0900

    fix

---
+++ nlppreprocess/flatten.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+import utils
+
+def run(path_in, path_out):
+    print("[nlppreprocess.flatten] Processing ...")
+    print("[nlppreprocess.flatten] IN: %s" % path_in)
+    print("[nlppreprocess.flatten] OUT : %s" % path_out)
+    iterator = utils.read_sentences(path_in)
+    print("[nlppreprocess.flatten] Writing ...")
+    with open(path_out, "w") as f:
+        is_begin = True
+        for s in iterator:
+            for w in s:
+                if is_begin:
+                    f.write("%s" % w)
+                    is_begin = False
+                else:
+                    f.write(" %s" % w)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str, required=True)
+    parser.add_argument("--output", type=str, required=True)
+    args = parser.parse_args()
+
+    path_in = args.input
+    path_out = args.output
+
+    run(path_in, path_out)
